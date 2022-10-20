3925387c20c8df8e2100e559e4f95e4ed4f22f9c
Alicia809 <114635078+Alicia809@users.noreply.github.com>
2022-10-19 21:59:58 -0600

    NetBeans_Login1

---
+++ Login/src/login/Login.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login;
+
+/**
+ *
+ * @author Alicia Ulloa
+ */
+public class Login {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+}
